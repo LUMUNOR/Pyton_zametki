d7fb4d915cd5a3598af166a6673124633f328850
LUMUNOR <Lukik884@gmailcom>
2024-01-11 16:13:40 +0300

    Начал создание класса Зааметка

--- Zametka.py
+++ Zametka.py
@@ -1,5 +1,20 @@
-class Zametka (object):
-    int id
-    string zagolovok
-    string telo
-    LokalData data
+class Zametka ():
+    def __init__(self, id, heading, body, date) -> None:
+        self._id = id # Идентификатор заметки
+        self._heading = heading # Заголовок
+        self._body = body # Тело заметки
+        self._date = date # Дата и время последнего изменения
+
+    @property
+    def get_id(self):
+        return self._id
+    @property
+    def get_heading(self):
+        return self._heading
+    @property
+    def get_body(self):
+        return self._body
+    @property
+    def get_date(self):
+        return self._date
+    
